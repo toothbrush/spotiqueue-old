2d4b17a4746b44b8e37ca14c2f7870f2a1a50897
Paul van der Walt <paul@denknerd.org>
2012-12-22 22:50:53 +0100

    Initial Commit

---
+++ Spotiqueue/DNAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  DNAppDelegate.h
+//  Spotiqueue
+//
+//  Created by Paul van der Walt on 22/12/2012.
+//  Copyright (c) 2012 denknerd.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DNAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Spotiqueue/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Spotiqueue
+//
+//  Created by Paul van der Walt on 22/12/2012.
+//  Copyright (c) 2012 denknerd.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
