fd977b258743e1b39cd4742a67e628022ffe47da
Paul van der Walt <paul@denknerd.org>
2013-01-23 18:50:37 +0100

    new class to make apple-l map to apple-f

---
+++ Spotiqueue/DNApplication.h
@@ -0,0 +1,13 @@
+//
+//  DNApplication.h
+//  Spotiqueue
+//
+//  Created by Paul van der Walt on 23/01/2013.
+//  Copyright (c) 2013 denknerd.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DNApplication : NSApplication
+
+@end
---
+++ Spotiqueue/DNApplication.m
@@ -0,0 +1,13 @@
+//
+//  DNApplication.m
+//  Spotiqueue
+//
+//  Created by Paul van der Walt on 23/01/2013.
+//  Copyright (c) 2013 denknerd.org. All rights reserved.
+//
+
+#import "DNApplication.h"
+
+@implementation DNApplication
+
+@end
