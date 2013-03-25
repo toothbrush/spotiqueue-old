19a4b9e4960ccf52054bc78c22030dab8c827387
Paul van der Walt <paul@denknerd.org>
2013-03-25 11:23:45 +0100

    add menu for sending to a friend -- not yet working.

---
+++ Spotiqueue/DNDropDownButton.h
@@ -0,0 +1,15 @@
+//
+//  DNDropDownButton.h
+//  Spotiqueue
+//
+//  Created by Paul van der Walt on 13/03/13.
+//  Copyright (c) 2013 denknerd.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DNDropDownButton : NSButton
+
+@property (nonatomic, retain) IBOutlet NSMenu* menu;
+
+@end
